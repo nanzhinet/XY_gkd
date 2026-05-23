c9e401af063d5bb77fee4b30a5be90dcf6935787
nanzhinet <3462456579@qq.com>
2026-05-23 15:56:03 +0800

    fix: 红果免费短剧 全屏广告-红果短剧广告自动上滑

---
+++ src/apps/com.phoenix.read.ts
@@ -0,0 +1,33 @@
+import { defineGkdApp } from '@gkd-kit/define';
+export default defineGkdApp({
+  id: 'com.phoenix.read',
+  name: '红果免费短剧',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-红果短剧广告自动上滑',
+      activityIds: [
+        'com.dragon.read.component.shortvideo.impl.ShortSeriesActivity',
+      ],
+      rules: [
+        {
+          name: '检测到「上滑继续观看短剧」时自动上滑',
+          fastQuery: true,
+          matches: '[vid="ank"][text="上滑继续观看短剧"][visibleToUser=true]',
+          swipeArg: {
+            start: {
+              x: 'screenWidth/2',
+              y: 'screenHeight*0.8',
+            },
+            end: {
+              x: 'screenWidth/2',
+              y: 'screenHeight*0.2',
+            },
+            duration: 300,
+          },
+          snapshotUrls: 'https://i.gkd.li/i/27098752',
+        },
+      ],
+    },
+  ],
+});
