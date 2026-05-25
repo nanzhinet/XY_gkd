ed49d993f8cb483fcb92206309e76d28e67eacb8
nanzhinet <155802941+nanzhinet@users.noreply.github.com>
2026-05-26 01:08:22 +0800

    feat: 橘汁 全屏广告

--- src/apps/com.lxf.snzlcgtzxyx.ts
+++ src/apps/com.lxf.snzlcgtzxyx.ts
@@ -18,5 +18,31 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '分段广告',
+      fastQuery: true,
+      activityIds: [
+        'com.hive.MainTabActivity',
+        'com.bytedance.sdk.openadsdk.core.activity.base.TTDelegateActivity',
+      ],
+      rules: [
+        {
+          key: 0,
+          name: '①x掉',
+          matches:
+            '@Image[width<66 && height<63] < [childCount=1] - [childCount=4] <<2 * <2 WebView <<8 [vid="ad_feed_content"]',
+          snapshotUrls: 'https://i.gkd.li/i/28220975',
+          exampleUrls: 'https://e.gkd.li/f0b45bb2-6d8f-4368-9ba8-d5cd8ec127fa',
+        },
+        {
+          preKeys: 0,
+          name: '②点击"无法关闭"',
+          matches: '@[clickable=true] > [text="无法关闭"]',
+          snapshotUrls: 'https://i.gkd.li/i/28221149',
+          exampleUrls: 'https://e.gkd.li/da65d359-eba0-4d76-b0fc-c01ec7063fa0',
+        },
+      ],
+    },
   ],
 });
